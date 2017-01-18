609efc31547e1e8d2938395b7f6e811b719271f3
enghks1414 <doohwanlee@doohwans-Mac.local>
2017-01-19 03:36:15 +0900

    version2

---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by doohwan Lee on 2017. 1. 19..
+//  Copyright © 2017년 doohwan Lee. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+}
