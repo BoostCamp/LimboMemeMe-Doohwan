9c86b701f650eed67f357d0acf3d04b9dc31e872
enghks1414 <doohwanlee@doohwans-Mac.local>
2017-01-28 16:22:12 +0900

    first commit

---
+++ MemeMe/MemeMeDrawView.swift
@@ -0,0 +1,21 @@
+//
+//  MemeMeDrawView.swift
+//  MemeMe
+//
+//  Created by doohwan Lee on 2017. 1. 26..
+//  Copyright © 2017년 doohwan Lee. All rights reserved.
+//
+
+import UIKit
+
+class MemeMeDrawView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
